6185e453eac75619934b3e275fe0cf32c0c4c434
Keenah <kvancamp@mail.com>
2021-10-21 00:59:58 -0500

    Done
    Added associations

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,35 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def genre_name=(name) #setter method (is called whenever a genre is initialized)
+    genre = Genre.find_or_create_by(name: name)
+    self.genre = genre
+  end
+
+  def genre_name #getter method
+    self.try(:genre).try(:name)
+  end
+
+  def artist_name
+    self.try(:artist).try(:name)
+  end
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+  end
+
+  def note_contents=(notes)
+    notes.each do |content|
+      if content.strip != ''
+        self.notes.build(content: content)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+  end
 end
